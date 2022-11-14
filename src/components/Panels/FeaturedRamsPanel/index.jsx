d0f8548aca35b49a956964839cb223f4c0f35508
Tammy Law <tlaw1@uci.edu>
2022-11-13 21:04:42 -0800

    second commit

--- src/components/Panels/FeaturedRamsPanel/index.jsx
+++ src/components/Panels/FeaturedRamsPanel/index.jsx
@@ -14,15 +14,15 @@ function FeaturedRamsPanel() {
         <Grid fluid>
           <Row>
             <Col xs={24} sm={24} md={8} lg={8} className="blackram">
-              <ProductCard imagePath="../../../../public/images/black_ram1.jpg"
+              <ProductCard imagePath="images/black_ram1.jpg"
                 name="Black Ram" price="34.99" productPath="/product/1/black-ram" />
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <ProductCard imagePath="../../../../public/images/veiltail_electric_blue_ram1.jpg"
+              <ProductCard imagePath="images/veiltail_electric_blue_ram1.jpg"
                 name="Veiltail Electric Blue Ram" price="24.99" productPath="/product/3/veiltail-electric-blue-ram" />
             </Col>
             <Col xs={24} sm={12} md={8}>
-              <ProductCard imagePath="../../../../public/images/gold_ram1.jpg"
+              <ProductCard imagePath="images/gold_ram1.jpg"
                 name="Gold Ram" price="9.99" productPath="/product/2/gold-ram" />
             </Col>
           </Row>
