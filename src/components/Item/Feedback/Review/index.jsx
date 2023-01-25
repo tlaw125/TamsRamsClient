d9271b65f6f77cb72e192e6d2c0771525fd6f96a
T L <tammylaw125@gmail.com>
2023-01-24 21:54:23 -0800

    added review cut off on display

--- src/components/Item/Feedback/Review/index.jsx
+++ src/components/Item/Feedback/Review/index.jsx
@@ -4,10 +4,18 @@ import "./index.css";
 
 function Review(props) {
 
+    let review_text = "";
+    if(props.text.length > 250){
+        review_text = props.text.slice(0,250) + "...";
+    }
+    else{
+        review_text = props.text;
+    }
+
     const ReviewInstance = () => {
         return (
             <div className="review-container">
-                <div className="review-text">{props.text}</div>
+                <div className="review-text">{review_text}</div>
                 <div className="rating-and-reviewer">
                     <Rate readOnly defaultValue={props.rating} color="blue" size="xs" />
                     <br />
